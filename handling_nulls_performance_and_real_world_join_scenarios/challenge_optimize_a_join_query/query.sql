f0721c018782014d63139e519e55005b70cf0890
KristenSF <>
2026-04-07 18:51:48 +0000

    Challenge: Optimize a Join Query

--- handling_nulls_performance_and_real_world_join_scenarios/challenge_optimize_a_join_query/query.sql
+++ handling_nulls_performance_and_real_world_join_scenarios/challenge_optimize_a_join_query/query.sql
@@ -1,21 +1,14 @@
--- Original (unoptimized) query for reference:
--- SELECT students.name, classes.class_name, teachers.name, grades.subject, grades.score
--- FROM students
--- JOIN classes ON students.class_id = classes.class_id
--- JOIN teachers ON classes.teacher_id = teachers.teacher_id
--- JOIN grades ON students.student_id = grades.student_id
--- WHERE grades.score > 80;
-
 SELECT
     s.student_id,
     s.name AS student_name,
     c.class_name,
-    t.name  AS teacher_name,
+    t.name AS teacher_name,
     g.subject,
     g.score
-FROM students AS s
-JOIN classes AS c on s.class_id = c.class_id
-JOIN teachers AS t ON c.teacher_id = t.teacher_id
-JOIN grades AS g ON s.student_id = g.student_id    
-WHERE g.score > 80;
-
+FROM
+    students s
+    INNER JOIN classes c ON s.class_id = c.class_id
+    INNER JOIN teachers t ON c.teacher_id = t.teacher_id
+    INNER JOIN grades g ON s.student_id = g.student_id
+WHERE
+    g.score > 80;
