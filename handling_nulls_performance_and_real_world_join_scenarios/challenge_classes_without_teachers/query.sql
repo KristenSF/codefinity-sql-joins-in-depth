34b0a4e0c8d0321f7c7bb263d4df7789aee74a35
KristenSF <>
2026-04-07 20:34:50 +0000

    Challenge: Classes Without Teachers

---
+++ handling_nulls_performance_and_real_world_join_scenarios/challenge_classes_without_teachers/query.sql
@@ -0,0 +1,9 @@
+SELECT 
+    c.class_id,
+    c.class_name AS class_name,
+    COALESCE(t.name,'Unassigned') AS teacher_name
+FROM classes c
+LEFT JOIN teachers t ON c.teacher_id = t.teacher_id;
+
+
+    
